e892e6be1620af691d8383056e961e42209e8080
TwiggyBeast021 <139478900+TwiggyBeast021@users.noreply.github.com>
2023-07-20 03:15:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,87 @@
-// Define the skill tree structure
-const skillTree = {
-  skills: {
-    attack: {
-      unlocked: true,
-      cost: 0,
-      dependencies: []
-    },
-    defense: {
-      unlocked: false,
-      cost: 1,
-      dependencies: []
-    },
-    magic: {
-      unlocked: false,
-      cost: 1,
-      dependencies: []
-    },
-    agility: {
-      unlocked: false,
-      cost: 1,
-      dependencies: []
-    },
-    sword: {
-      unlocked: true,
-      cost: 0,
-      dependencies: ['attack']
-    },
-    dagger: {
-      unlocked: false,
-      cost: 1,
-      dependencies: ['attack']
-    },
-    // Add more skills as needed
-  },
-  skillPoints: 0
-};
+// Sample skill tree data (with updated structure)
+const skillTreeData = [
+    { id: 1, name: "Skill 1", description: "Description of Skill 1", unlocked: true, prerequisite: null, x: 0, y: 0 },
+    { id: 2, name: "Skill 2", description: "Description of Skill 2", unlocked: false, prerequisite: 1, x: 200, y: -150 },
+    { id: 3, name: "Skill 3", description: "Description of Skill 3", unlocked: false, prerequisite: 1, x: -200, y: -150 },
+    { id: 4, name: "Skill 4", description: "Description of Skill 4", unlocked: false, prerequisite: 2, x: 300, y: -300 },
+    { id: 5, name: "Skill 5", description: "Description of Skill 5", unlocked: false, prerequisite: 2, x: 100, y: -300 },
+    // Add more skills here...
+];
 
-// Function to unlock a skill
-function unlockSkill(skillName) {
-  const skill = skillTree.skills[skillName];
+const skillTreeContainer = document.getElementById('skillTree');
+const gatherButton = document.getElementById('gatherButton');
+const progressBar = document.getElementById('progressBar');
 
-  // Check if the skill is already unlocked or if there are insufficient skill points
-  if (skill.unlocked || skillTree.skillPoints < skill.cost) {
-    console.log('Cannot unlock skill:', skillName);
-    return;
-  }
+// Generate Skill Tree
+function generateSkillTree() {
+    skillTreeContainer.innerHTML = '';
+    skillTreeData.forEach(skill => {
+        if (skill.unlocked || skill.prerequisite === null) {
+            const skillElement = document.createElement('div');
+            skillElement.className = 'skill';
+            skillElement.innerText = skill.unlocked ? skill.name : 'Locked';
+            skillElement.title = skill.unlocked ? skill.description : 'Must progress further';
+            skillElement.style.backgroundColor = skill.unlocked ? '#6b6b6b' : '#444';
+            skillElement.style.left = skill.x + 'px';
+            skillElement.style.top = skill.y + 'px';
 
-  // Check if all the skill's dependencies are unlocked
-  for (const dependency of skill.dependencies) {
-    if (!skillTree.skills[dependency].unlocked) {
-      console.log('Cannot unlock skill:', skillName, 'Dependency not met:', dependency);
-      return;
-    }
-  }
+            skillElement.addEventListener('click', () => {
+                if (skill.unlocked) {
+                    // Handle skill selection (you can add relevant logic here)
+                    console.log(`Selected Skill: ${skill.name}`);
+                }
+            });
 
-  // Deduct skill points and unlock the skill
-  skillTree.skillPoints -= skill.cost;
-  skill.unlocked = true;
-  console.log('Skill unlocked:', skillName);
+            skillTreeContainer.appendChild(skillElement);
+        }
 
-  // Update the skill tree UI
-  const skillElement = document.getElementById(skillName);
-  skillElement.classList.add('unlocked');
-  skillElement.querySelector('.unlock-btn').disabled = true;
-  document.getElementById('skill-points').textContent = skillTree.skillPoints;
+        if (skill.prerequisite) {
+            const skillConnection = document.createElement('div');
+            skillConnection.className = 'skill-connection';
+            skillConnection.style.width = '2px';
+            skillConnection.style.height = Math.sqrt((skill.x * skill.x) + (skill.y * skill.y)) + 'px';
+            skillConnection.style.left = (skill.x + 50) + 'px';
+            skillConnection.style.top = (skill.y + 50) + 'px';
+            skillConnection.style.transformOrigin = '0 0';
+            skillConnection.style.transform = `rotate(${getRotation(skill.x, skill.y)}deg)`;
+            skillTreeContainer.appendChild(skillConnection);
+        }
+    });
 }
 
-// Add event listeners to the unlock buttons
-const unlockButtons = document.querySelectorAll('.unlock-btn');
-unlockButtons.forEach(button => {
-  button.addEventListener('click', function() {
-    const skillName = this.parentNode.id;
-    unlockSkill(skillName);
-  });
-});
+// Calculate rotation for skill connections
+function getRotation(x, y) {
+    return Math.atan2(y, x) * (180 / Math.PI);
+}
 
-// Example usage
-skillTree.skillPoints = 10;
-document.getElementById('skill-points').textContent = skillTree.skillPoints;
+// Update Skill Tree based on unlocked skills
+function updateSkillTree() {
+    skillTreeData.forEach(skill => {
+        const prevSkill = skill.prerequisite ? skillTreeData.find(s => s.id === skill.prerequisite) : null;
+        if (prevSkill) {
+            skill.unlocked = prevSkill.unlocked;
+        }
+    });
+}
+
+// Gather Flowers and update the progress bar
+let progressBarValue = 0;
+
+function gatherFlowers() {
+    progressBarValue++;
+    progressBar.value = progressBarValue;
+
+    if (progressBarValue === 15) {
+        progressBarValue = 0;
+        progressBar.value = 0;
+        // Add a skill point here and unlock relevant skills based on player progression
+        updateSkillTree();
+        generateSkillTree();
+    }
+}
+
+// Attach event listeners
+gatherButton.addEventListener('click', gatherFlowers);
+
+// Initial setup
+generateSkillTree();
