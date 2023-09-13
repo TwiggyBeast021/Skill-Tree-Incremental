b7fc478bb85f1c3295012c0890cc6f6f82ea0ebb
TwiggyBeast021 <139478900+TwiggyBeast021@users.noreply.github.com>
2023-09-13 13:14:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,91 @@
-body {
-  font-family: sans-serif;
-  margin: 0;
-  padding: 0;
+/* script.js */
+
+var skills = [
+  {
+    name: "Strength",
+    cost: 100,
+    progress: 0,
+    description: "Increases your physical strength."
+  },
+  {
+    name: "Agility",
+    cost: 150,
+    progress: 0,
+    description: "Increases your reflexes and dexterity."
+  },
+  {
+    name: "Intelligence",
+    cost: 200,
+    progress: 0,
+    description: "Increases your mental acuity and wisdom."
+  },
+  {
+    name: "Charisma",
+    cost: 250,
+    progress: 0,
+    description: "Increases your ability to influence others."
+  },
+  {
+    name: "Wisdom",
+    cost: 300,
+    progress: 0,
+    description: "Increases your understanding of the world around you."
+  },
+  {
+    name: "Luck",
+    cost: 350,
+    progress: 0,
+    description: "Increases your chances of success."
+  },
+  {
+    name: "Endurance",
+    cost: 400,
+    progress: 0,
+    description: "Increases your stamina and resistance to fatigue."
+  },
+  {
+    name: "Perception",
+    cost: 450,
+    progress: 0,
+    description: "Increases your awareness and observation skills."
+  },
+  {
+    name: "Willpower",
+    cost: 500,
+    progress: 0,
+    description: "Increases your mental strength and resistance to temptation."
+  }
+];
+
+var selectedSkillIndex = 0;
+
+function updateSkillProgress() {
+  var skill = skills[selectedSkillIndex];
+  skill.progress += 1;
+
+  if (skill.progress >= skill.cost) {
+    skill.progress = 0;
+    selectedSkillIndex++;
+  }
 }
 
-.container {
-  width: 500px;
-  margin: 0 auto;
-}
+window.onload = function() {
+  var container = document.querySelector(".container");
 
-.skill-tree {
-  display: flex;
-  flex-direction: column;
-}
+  for (var i = 0; i < skills.length; i++) {
+    var skill = skills[i];
 
-.skill {
-  margin-bottom: 10px;
-}
+    var skillElement = document.createElement("div");
+    skillElement.classList.add("skill");
 
-.skill-name {
-  font-size: 18px;
-  font-weight: bold;
-}
+    var skillNameElement = document.createElement("h2");
+    skillNameElement.textContent = skill.name;
 
-.skill-cost {
-  font-size: 14px;
-}
+    var skillCostElement = document.createElement("p");
+    skillCostElement.textContent = "Cost: " + skill.cost;
 
-.skill-progress {
-  width: 100%;
-  height: 20px;
-  background-color: #ccc;
-  border-radius: 5px;
-}
+    var skillProgressElement = document.createElement("div");
+    skillProgressElement.classList.add("skill-progress");
 
-.skill-progress-bar {
-  width: 0%;
-  height: 100%;
-  background-color: #000;
-  border-radius: 5px;
-}
-
-.skill-description {
-  font-size: 12px;
-}
-
-.skill-max {
-  text-decoration: line-through;
-}
+    var skillProgressBarElement = document.createElement("div");
+    skillProgressBarElement.classList.add("skill-progress-
